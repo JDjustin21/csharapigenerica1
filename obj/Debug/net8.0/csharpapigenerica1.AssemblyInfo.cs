d3c61f5357547da315bcd7f42837d93b211457ce
Justin Villada <jdpavi0821@gmail.com>
2025-03-07 12:39:29 -0500

    Agregar la base de datos

--- obj/Debug/net8.0/csharpapigenerica1.AssemblyInfo.cs
+++ obj/Debug/net8.0/csharpapigenerica1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharpapigenerica1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95e8efeba311b3f13e4d930bf9fd7f61d94e4895")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharpapigenerica1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharpapigenerica1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
